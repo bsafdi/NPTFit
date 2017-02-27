7dbacfe1ce323e62d4a763e8e888f2c58906c5e2
Siddharth Mishra Sharma <smsharma@feynman.princeton.edu>
2017-02-27 18:00:04 -0500

    Updated setup and x_m to make GSL install/use optional

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import logging
 from setuptools import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
@@ -8,22 +9,23 @@ extensions = [
         include_dirs=[numpy.get_include()], extra_compile_args=["-ffast-math",'-O3']),
     Extension("NPTFit.pll", ["NPTFit/pll.pyx"],
         include_dirs=[numpy.get_include()], extra_compile_args=["-ffast-math",'-O3']),
+    Extension("NPTFit.incgamma_fct_p", ["NPTFit/incgamma_fct_p.pyx"],
+        include_dirs=[numpy.get_include()], extra_compile_args=["-ffast-math",'-O3']),
+    Extension("NPTFit.x_m", ["NPTFit/x_m.pyx"],
+        include_dirs=[numpy.get_include()], extra_compile_args=["-ffast-math",'-O3']),
     Extension("NPTFit.incgamma_fct", ["NPTFit/incgamma_fct.pyx"],
         include_dirs=[numpy.get_include()], libraries=["gsl", "gslcblas", "m"],
-        extra_compile_args=["-ffast-math",'-O3']),
-    Extension("NPTFit.x_m", ["NPTFit/x_m.pyx"],
-        include_dirs=[numpy.get_include()], extra_compile_args=["-ffast-math",'-O3'])
+        extra_compile_args=["-ffast-math",'-O3'])
 ]
 
-setup(
-    name='NPTFit',
-    version='0.1.1',
-    description='A Python package for Non-Poissonian Template Fitting',
-    url='https://github.com/bsafdi/NPTFit',
-    author='Siddharth Mishra-Sharma',
-    author_email='smsharma@princeton.edu',
-    license='MIT',
-    install_requires=[
+setup_args = {'name':'NPTFit',
+    'version':'0.1.1',
+    'description':'A Python package for Non-Poissonian Template Fitting',
+    'url':'https://github.com/bsafdi/NPTFit',
+    'author':'Siddharth Mishra-Sharma',
+    'author_email':'smsharma@princeton.edu',
+    'license':'MIT',
+    'install_requires':[
             'numpy',
             'matplotlib',
             'healpy',
@@ -32,8 +34,15 @@ setup(
             'jupyter',
             'corner',
             'mpmath',
-        ],
-
-    packages=['NPTFit'],
-    ext_modules = cythonize(extensions)
-)
+        ]}
+        
+try:
+    setup(packages=['NPTFit'],
+        ext_modules = cythonize(extensions),
+        **setup_args
+    )
+except:
+    setup(packages=['NPTFit'],
+        ext_modules = cythonize(extensions[:-1]),
+        **setup_args
+    )
