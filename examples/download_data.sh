deda7f95e5fe32be4283cb50ca7c37267ecf01a3
Nick Rodd <nrodd@mit.edu>
2018-06-15 08:45:22 -0400

    Updated data to new dspace link

--- examples/download_data.sh
+++ examples/download_data.sh
@@ -1,3 +1,3 @@
-wget https://dspace.mit.edu/bitstream/handle/1721.1/105492/fermi_data.tar.gz
-tar -xvf fermi_data.tar.gz
+wget https://dspace.mit.edu/bitstream/handle/1721.1/105492/fermi_data.tar.gz?sequence=4
+tar -xvf fermi_data.tar.gz?sequence=4
 rm -r fermi_data.tar.gz*
